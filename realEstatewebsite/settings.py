8ffa10b34861221941c63a5d92dc37d3258f5fe0
Camilool8 <josejogag@live.com>
2023-07-11 19:58:40 -0400

    beta v2 - realEstateWebsite

--- realEstatewebsite/settings.py
+++ realEstatewebsite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*os8s^r1-b1&tvn7@7v_01-_5#go_k%rhovoa(#^47bd(ofyg7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
